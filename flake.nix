8e156d9398fefc7b1e6d1d19e2a63f94ea8177ff
ada <adanoelleyoung@gmail.com>
2024-11-04 14:04:30 -0500

    feat: Add uv

--- flake.nix
+++ flake.nix
@@ -13,9 +13,11 @@
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages = with pkgs; [ 
+        comma
         fastfetch
         helix
         hyfetch
+        uv
         vim
       ];
 
