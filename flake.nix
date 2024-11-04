2796f23d1a89721d34f6bb0e8154c6bae3cbccc0
Ada Young <ada@bubblegum.local>
2024-11-04 15:36:25 -0500

    refactor: Create system configuration module

--- flake.nix
+++ flake.nix
@@ -3,69 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    darwin.url = "github:LnL7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, home-manager, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages = with pkgs; [ 
-        comma
-        fastfetch
-        helix
-        hyfetch
-        uv
-        vim
-      ];
-
-      users.users.ada = {
-        name = "ada";
-        home = "/Users/ada";
+  outputs = inputs@{ self, home-manager, darwin, nixpkgs }: {
+    darwinConfigurations = {
+      bubblegum = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [ 
+          ./system/configuration.nix
+          home-manager.darwinModules.home-manager 
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.ada = import ./user/ada/home.nix;
+          }
+        ];
       };
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      programs.zsh.enable = true;
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."bubblegum".pkgs;
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#bubblegum
-    darwinConfigurations."bubblegum" = nix-darwin.lib.darwinSystem {
-      modules = [ 
-        configuration 
-        home-manager.darwinModules.home-manager 
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.ada = import ./user/ada/home.nix;
-        }
-      ];
-    };
-
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."bubblegum".pkgs;
   };
 }
---
+++ system/configuration.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }: 
+
+{
+  environment.systemPackages = with pkgs; [
+    comma
+    fastfetch
+    helix
+    hyfetch
+    uv
+    vim
+  ];
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # Enable alternative shell support in nix-darwin.
+  programs.zsh.enable = true;
+
+  # Set Git commit hash for darwin-version.
+  #system.configurationRevision = self.rev or self.dirtyRev or null;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 5;
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  users.users.ada = {
+    name = "ada";
+    home = "/Users/ada";
+  };
+}
